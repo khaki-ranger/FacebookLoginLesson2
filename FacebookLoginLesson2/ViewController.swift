7174bb96a27b55e85f8cd0ae88f150b196b1b489
Terashima <yamanogomora@gmail.com>
2019-03-15 17:24:53 +0900

    Add: ログイン後の画面のレイアウトと画面遷移の処理を実装

--- FacebookLoginLesson2/ViewController.swift
+++ FacebookLoginLesson2/ViewController.swift
@@ -7,19 +7,65 @@
 //
 
 import UIKit
+import FBSDKCoreKit
+import FBSDKLoginKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, FBSDKLoginButtonDelegate {
 
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
     }
-
+    
+    // 画面遷移
+    func segueToSeccondViewController() {
+        self.performSegue(withIdentifier: "toSecondViewController", sender: nil)
+    }
+    
+    override func viewDidAppear(_ animated: Bool) {
+        // ログイン済みかチェック
+        if let _ = FBSDKAccessToken.current() {
+            // 画面遷移
+            // performSegue(withIdentifier: "modalTop", sender: self)
+            print("ログインしてます")
+            segueToSeccondViewController()
+        } else {
+            // FBログインボタンを設置
+            let fbLoginBtn = FBSDKLoginButton()
+            fbLoginBtn.readPermissions = ["public_profile", "email"]
+            fbLoginBtn.center = self.view.center
+            fbLoginBtn.delegate = self
+            self.view.addSubview(fbLoginBtn)
+        }
+    }
+    
     override func didReceiveMemoryWarning() {
         super.didReceiveMemoryWarning()
         // Dispose of any resources that can be recreated.
     }
-
+    
+    // ログインのコールバック
+    func loginButton(_ loginButton: FBSDKLoginButton!, didCompleteWith result: FBSDKLoginManagerLoginResult!, error: Error!) {
+        // エラーチェック
+        if error == nil {
+            // キャンセルしたかどうか
+            if result.isCancelled {
+                print("キャンセル")
+            } else {
+                // 画面遷移
+                // performSegue(withIdentifier: "modalTop", sender: self)
+                print("ログインしました")
+                segueToSeccondViewController()
+            }
+        } else {
+            print("エラー")
+        }
+    }
+    
+    // ログアウトのコールバック
+    func loginButtonDidLogOut(_ loginButton: FBSDKLoginButton!) {
+        
+    }
 
 }
 
